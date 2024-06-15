72c931ea013564708f967c93d115327976037407
Drkainat8 <161299000+Drkainat8@users.noreply.github.com>
2024-06-15 09:42:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// 25 assignment
+// creat variable with name alien color assign it a value green,red or yellow
+var allien_color = "green";
+//write an if statment weather the alien color is green if it is print message that he earned 5 points
+if (allien_color == "green") {
+    console.log(" the player just earned 5 points");
+}
+//write one more version of if statment in which there will bw no output
+allien_color = "red";
+if (allien_color == "green") {
+    console.log("the player just earned 10 points");
+}
---
+++ main.ts
@@ -0,0 +1,14 @@
+// 25 assignment
+// creat variable with name alien color assign it a value green,red or yellow
+let allien_color : string = `green`;
+//write an if statment weather the alien color is green if it is print message that he earned 5 points
+if(allien_color==`green`){
+
+console.log(` the player just earned 5 points`);
+}
+//write one more version of if statment in which there will no output
+allien_color=`red`;
+if(allien_color ==`green`){
+
+   console.log(`the player just earned 10 points`);
+}
